d153900c0a1d8f33f855324213d5efc4a1651c63
alex-pythonista <indrajit.alex@gmail.com>
2022-11-17 23:27:27 +0600

    Scaffold init

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 // void main() {
 //   runApp(HelloApp());
 // }
+
 void main() => runApp(HelloApp());
 
 class HelloApp extends StatelessWidget {
-  
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World!!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Meghar mon kharap!"),
+        ),
+        body: Text("মেঘা এখনো তাক্কুকে মিস করে"),
+      ),
     );
   }
 }
