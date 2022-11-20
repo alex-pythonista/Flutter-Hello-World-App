8bc1b31f03649b1f0d10b4842d47d5ff06487f6d
alex-pythonista <indrajit.alex@gmail.com>
2022-11-20 22:56:24 +0600

    quiz app init

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,36 @@ void main() => runApp(HelloApp());
 
 class HelloApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    var questions = [
+      "What is your favorite color?",
+      "What is your favorite sports?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Meghar mon kharap!"),
         ),
-        body: Text("মেঘা এখনো তাক্কুকে মিস করে"),
+        body: Column(
+          children: [
+            Text("The question!"),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Answer 1"),
+            ),
+          ],
+        ),
       ),
     );
   }
