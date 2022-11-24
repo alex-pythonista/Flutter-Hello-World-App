447b62543586e366c605d78e8d6b45b913711394
alex-pythonista <indrajit.alex@gmail.com>
2022-11-24 17:38:30 +0600

    if-else and ternary operations

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,29 @@ class HelloApp extends StatefulWidget {
 
 class _HelloAppState extends State<HelloApp> {
   var _questionIndex = 0;
+  static const questions = [
+    {
+      "questionText": "What is your favorite color?",
+      "answers": ["Cyan", "Crimson Red", "Magenta", "Purple"],
+    },
+    {
+      "questionText": "Who is your favorite footballer?",
+      "answers": ["Neymar Jr", "Ronaldo", "Messi", "MBappe"],
+    },
+    {
+      "questionText": "Who do you think win this worldcup?",
+      "answers": ["Argentina", "Brazil", "Germany", "Ghana"]
+    }
+  ];
   void _answerQuestion() {
     setState(() {
       _questionIndex++;
     });
+    print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print("We have more questions!");
+    } else
+      print("No more questions!");
 
     // ignore: avoid_print
     print(_questionIndex);
@@ -30,35 +49,25 @@ class _HelloAppState extends State<HelloApp> {
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        "questionText": "What is your favorite color?",
-        "answers": ["Cyan", "Crimson Red", "Magenta", "Purple"],
-      },
-      {
-        "questionText": "Who is your favorite footballer?",
-        "answers": ["Neymar Jr", "Ronaldo", "Messi", "MBappe"],
-      },
-      {
-        "questionText": "Who do you think win this worldcup?",
-        "answers": ["Argentina", "Brazil", "Germany", "Ghana"]
-      }
-    ];
-    print(questions[_questionIndex]["questionText"]);
+    // print(questions[_questionIndex]["questionText"]);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Quiz App!"),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]["questionText"] as String),
-            ...(questions[_questionIndex]["answers"] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]["questionText"] as String),
+                  ...(questions[_questionIndex]["answers"] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text("You did it!"),
+              ),
       ),
     );
   }
